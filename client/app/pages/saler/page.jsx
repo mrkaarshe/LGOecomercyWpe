0d7420c19bf9927878f5dfcfc5a1b49395fd6be6
mrkaarshe <odaykaarshe@gmail.com>
2025-12-24 18:10:09 +0300

    added dashboard Laout

--- client/app/pages/saler/page.jsx
+++ client/app/pages/saler/page.jsx
@@ -4,11 +4,12 @@ import React from 'react'
 import { useState } from 'react'
 import Saler from '../../../components/Saler'
 import AddProducts from '@/components/AddProducts'
+import SallerDashboard from '@/components/SallerDashboard'
 const page = () => {
     const [isopen ,setIsopen] = useState(false)
   return (
-    <div className='overflow-hidden'>
-      <AddProducts/>
+    <div className='overflow-x-hidden'>
+      <SallerDashboard/>
     </div>
    
   )
--- client/components/Saler.jsx
+++ client/components/Saler.jsx
@@ -6,39 +6,39 @@ import { useState } from 'react'
 const page = () => {
     const [isopen ,setIsopen] = useState(false)
   return (
-    <div className={`-translate-x-22  md:translate-0 ${isopen ? "translate-x-0" : ""}  bg-white/95 bottom-0 absolute top-20 md:top-2  left-0  md:relative  min-h-[90vh]`}>
-        <aside  className={`${isopen ? "w-60 transition-all" : "w-[100px]"}  relative  min-h-screen w-60 border-r mt-4 gap-3  rounded-tr-2xl flex flex-col  p-4`}>
-            <div onClick={()=> setIsopen(!isopen)} className=' border-white/10 absolute  top-1/2 -right-10 bg-[#333] p-5 rounded-full'>
+    <div className={`-translate-x-28  md:translate-0 ${isopen ? "translate-x-0" : ""}  bg-white/95 bottom-0 absolute top-20 md:top-2 z-50  left-2  md:relative  min-h-[90vh]`}>
+        <aside  className={`${isopen ? "w-60 transition-all rounded-2xl " : "w-[100px] rounded-full"}  relative  min-h-[95%] w-60 border-r mt-2 gap-3 bg-black shadow-2xl   flex flex-col  p-4.5`}>
+            <div onClick={()=> setIsopen(!isopen)} className=' border-white/10 absolute z-100  top-1/2 -right-10 bg-[#333] p-5 rounded-full'>
                 <i className=' text-white'><ArrowRightLeft/></i>
             </div>
             <div className='  rounded-md '>
                 <Link href={'/pages/saler'} className="font-bold flex border-b  items-center text-2xl pb-4">
                     {isopen ? (
                         <>
-                        <img className="bg-black rounded-full w-15 p-3" src="/icon.png" />
-                        <span className="ml-2">LGO</span>
+                        <img className=" rounded-full w-15 p-3 ml-5" src="/icon.png" />
+                        <span className="ml-2 text-white">LGO</span>
                         </>
                     ) : (
-                        <img className="bg-black rounded-full w-15 p-3" src="/icon.png" />
+                        <img className="bg-[#333] rounded-full w-15 p-3" src="/icon.png" />
                     )}
                     </Link>
             </div>
             <div className=''>
                 <ul className='flex flex-col gap-3'>
-                    <Link href={'/pages/saler/addProduct'} className=' bg-black/99 hover:bg-black/10 p-4 text-white hover:text-black rounded-full font-bold flex gap-2 justify-cen items-center'>
+                    <Link href={'/pages/saler/addProduct'} className=' bg-[#333] hover:bg-black/10 p-4.5 text-white hover:text-black rounded-full font-bold flex gap-2 justify-cen items-center'>
                         <span>
                             <PackagePlus size={25}/>
                         </span>
                         <p className={`${isopen ? "block" :"hidden"}`}>Add Product</p>
                     </Link>
-                     <Link href={'/pages/saler/ProductsList'} className=' bg-black/99 hover:bg-black/10 p-4 text-white hover:text-black rounded-full font-bold flex gap-2 justify-cen items-center'>
+                     <Link href={'/pages/saler/ProductsList'} className=' bg-[#333] hover:bg-black/10 p-4.5 text-white hover:text-black rounded-full font-bold flex gap-2 justify-cen items-center'>
                         <span>
                             <List size={25}/>
                         </span>
                         <p className={`${isopen ? "block" :"hidden"}`}>Products List</p>
 
                     </Link>
-                     <Link href={'/pages/saler/Orders'} className=' bg-black/99 hover:bg-black/10 p-4 text-white hover:text-black rounded-full font-bold flex gap-2 justify-cen items-center'>
+                     <Link href={'/pages/saler/Orders'} className=' bg-[#333] hover:bg-black/10 p-4.5 text-white hover:text-black rounded-full font-bold flex gap-2 justify-cen items-center'>
                         <span>
                             <ShoppingBag size={25}/>
                         </span>
